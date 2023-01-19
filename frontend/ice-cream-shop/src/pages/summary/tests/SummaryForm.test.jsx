6e36c9121bac0d7f739f136534fc3fb7cd1e7450
anitkumarsingh <anikumarsingh99@gmail.com>
2023-01-19 14:01:16 +0530

    replace fireEvent with userEvent

--- frontend/ice-cream-shop/src/pages/summary/tests/SummaryForm.test.jsx
+++ frontend/ice-cream-shop/src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,15 +1,17 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 describe('Testing Summary form', () => {
-	test('Enabling and Disabling button on checking and unchecking checkbox', () => {
+	test('Enabling and Disabling button on checking and unchecking checkbox', async () => {
+		const user = userEvent.setup();
 		render(<SummaryForm />);
 		const orderSubmitBtn = screen.getByRole('button', { name: 'Confirm order' });
 		const termsAndConditionCheckbox = screen.getByRole('checkbox', {
 			name: 'I agree to Terms and Conditions'
 		});
 		expect(orderSubmitBtn).toBeDisabled();
-		fireEvent.click(termsAndConditionCheckbox);
+		await user.click(termsAndConditionCheckbox);
 		expect(orderSubmitBtn).toBeEnabled();
 	});
 });
